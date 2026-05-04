aa89a91c52b8a83a1a5269f2cf43ee096abaa037
Abhishek Singh <asdec0302@gmail.com>
2026-05-05 01:08:36 +0530

    Update light theme with warm beige and green palette

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -56,7 +56,7 @@ const App = () => {
         </Routes>
 
         <Toaster
-          position="top-right"
+          position="top-center"
           toastOptions={{
             style: {
               background: '#1a1d27',
